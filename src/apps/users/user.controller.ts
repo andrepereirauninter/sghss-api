489f786bc9df02c0d74d335abec5938f512388c1
André Pereira <--show-origin>
2025-08-30 17:57:15 -0300

    docs: add create user endpoint documentation

--- src/apps/users/user.controller.ts
+++ src/apps/users/user.controller.ts
@@ -1,6 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+import { apiResponses } from '../../common/constants/swagger';
 import { CreateUserPayload } from './payload/create-user.payload';
 import { UserService } from './user.service';
 
@@ -10,6 +16,15 @@ export class UserController {
   constructor(private readonly service: UserService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar um novo usuário.',
+  })
+  @ApiCreatedResponse({
+    description: 'Usuário criado com sucesso.',
+  })
+  @ApiBadRequestResponse({
+    description: apiResponses.badRequestWithValidation,
+  })
   create(@Body() payload: CreateUserPayload) {
     return this.service.create(payload);
   }
